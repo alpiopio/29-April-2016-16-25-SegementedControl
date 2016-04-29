e452602f8492b8ad6594181394cd2a64e9e16696
alfian <alfiansyah@go-jek.com>
2016-04-29 16:25:38 +0700

    Initial Commit

---
+++ 29-April-2016-16:25-SegementedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  29-April-2016-16:25-SegementedControl
+//
+//  Created by alfian on 29/04/2016.
+//  Copyright © 2016 alfian.official.organization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
